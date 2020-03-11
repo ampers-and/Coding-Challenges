a3d42603cb13212f964b9b31cb6fa120a707b8c4
ampersand <hossainazifa@gmail.com>
2020-03-10 23:59:03 -0700

    simple arr sum js

---
+++ arrSum.js
@@ -0,0 +1,13 @@
+// https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+function simpleArraySum(ar) {
+  /*
+   * Write your code here.
+   */
+
+  let sum = 0;
+  for (let i = 0; i < ar.length; i++) {
+    sum += ar[i];
+  }
+  return sum;
+}
