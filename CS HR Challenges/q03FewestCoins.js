970b20b75ed870fe7e6c3c44093033fb14ba77cd
ampersand <hossainazifa@gmail.com>
2020-03-11 22:02:12 -0700

    q3 update

--- CS HR Challenges/q03FewestCoins.js
+++ CS HR Challenges/q03FewestCoins.js
@@ -11,15 +11,25 @@
 function fewestCoins(coins) {
   // Write your code here
   let coinArr = coins.split("");
-  let coinSet = Set(coinArr);
+  let coinSet = new Set(coinArr);
+
+  console.log("coins: ", coins);
+  console.log("coinArr: ", coinArr);
+  console.log("coinSet: ", coinSet);
 
   let minLength = coinArr.length;
   let sbstr = [];
 
   for (let i = 0; i < coinArr.length; i++) {
-    for (let j = i + coinSet.size(); j < coinArr.length; j++) {
+    for (let j = i + coinSet.size - 1; j < coinArr.length; j++) {
       sbstr = coinArr.slice(i, j);
-      if (Set(sbstr) === coinSet) {
+      sbSet = new Set(sbstr);
+
+      console.log("i: ", i, "       j: ", j);
+      console.log("substr: ", sbstr);
+      console.log("sbSet: ", coiSet);
+
+      if (sbSet === coinSet) {
         if (sbstr.length < minLength) {
           minLength = sbstr.length;
         }
